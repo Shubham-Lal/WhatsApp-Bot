4abc7773574ba609e65b37ed89ef65892efc4767
Shubham Lal <im.shubhamlal@gmail.com>
2023-12-29 22:49:13 +0530

    setup basic WhatsApp Bot

---
+++ bot.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const qrcode = require('qrcode-terminal');
+const { Client, LocalAuth } = require('whatsapp-web.js');
+
+const client = new Client({
+    authStrategy: new LocalAuth()
+});
+
+client.on('qr', qr => {
+    qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', async message => {
+    if (message.from !== process.env.WHATSAPP_GROUP_ID) return;
+
+    const contact = await message.getContact();
+    const name = contact.pushname?.split(" ")[0];
+
+    const msg = message.body.toLowerCase();
+
+    switch (true) {
+        case containsGreeting(msg):
+            message.reply(`hii ${name}`);
+            break;
+        default:
+            break;
+    }
+});
+
+client.initialize();
+
+const containsGreeting = (msg) => {
+    return msg.includes("hello") || msg.includes("hi") || msg.includes("hey");
+};
