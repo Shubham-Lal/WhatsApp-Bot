083421c4161e2a34fa29eea51ad3e37910dd81ba
Shubham Lal <im.shubhamlal@gmail.com>
2023-12-30 00:40:22 +0530

    stop background process

--- bot.js
+++ bot.js
@@ -3,7 +3,12 @@ const qrcode = require('qrcode-terminal');
 const { Client, LocalAuth } = require('whatsapp-web.js');
 
 const client = new Client({
-    authStrategy: new LocalAuth()
+    authStrategy: new LocalAuth(),
+    puppeteer: {
+        args: [
+            '--no-sandbox'
+        ]
+    }
 });
 
 client.on('qr', qr => {
