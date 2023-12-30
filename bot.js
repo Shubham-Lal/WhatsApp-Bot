de420fb684518f964c8ea7fdfbb46616c143d37e
Shubham Lal <im.shubhamlal@gmail.com>
2023-12-31 00:33:18 +0530

    wip: add chrome path to puppeteer

--- bot.js
+++ bot.js
@@ -6,55 +6,56 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 const locateChrome = require('locate-chrome');
 
 try {
-    const executablePath = await new Promise(resolve => locateChrome(arg => resolve(arg)));
-    const client = new Client({
-        authStrategy: new LocalAuth(),
-        puppeteer: {
-            headless: true,
-            args: [
-                '--no-sandbox',
-                '--disable-setuid-sandbox'
-            ],
-            executablePath: executablePath
-        }
+    locateChrome(async function (l) {
+        const client = new Client({
+            authStrategy: new LocalAuth(),
+            puppeteer: {
+                headless: true,
+                args: [
+                    '--no-sandbox',
+                    '--disable-setuid-sandbox'
+                ],
+                executablePath: l
+            }
+        });
+
+        client.on('qr', qr => {
+            qrcode.generate(qr, { small: true });
+        });
+
+        client.on('ready', () => {
+            console.log('Client is ready!');
+        });
+
+        client.on('message', async message => {
+            // if (message.from !== process.env.WHATSAPP_GROUP_ID) return;
+
+            const contact = await message.getContact();
+            const name = contact.pushname?.split(" ")[0];
+
+            const msg = message.body.toLowerCase();
+
+            switch (true) {
+                case containsGreeting(msg):
+                    message.reply(`hii ${name}, how was your day?`);
+                    break;
+                default:
+                    break;
+            }
+        });
+
+        client.initialize();
+
+        app.get('/', (req, res) => {
+            res.send('Online👌');
+        })
+        const PORT = process.env.PORT || 8000;
+        app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
     });
-
-    client.on('qr', qr => {
-        qrcode.generate(qr, { small: true });
-    });
-
-    client.on('ready', () => {
-        console.log('Client is ready!');
-    });
-
-    client.on('message', async message => {
-        // if (message.from !== process.env.WHATSAPP_GROUP_ID) return;
-
-        const contact = await message.getContact();
-        const name = contact.pushname?.split(" ")[0];
-
-        const msg = message.body.toLowerCase();
-
-        switch (true) {
-            case containsGreeting(msg):
-                message.reply(`hii ${name}, how was your day?`);
-                break;
-            default:
-                break;
-        }
-    });
-
-    client.initialize();
-
-    app.get('/', (req, res) => {
-        res.send('Online👌');
-    })
-    const PORT = process.env.PORT || 8000;
-    app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
-
-    const containsGreeting = (msg) => {
-        return msg.includes("hello") || msg.includes("hi") || msg.includes("hey");
-    };
 } catch (error) {
     console.log(error);
 }
+
+const containsGreeting = (msg) => {
+    return msg.includes("hello") || msg.includes("hi") || msg.includes("hey");
+};
